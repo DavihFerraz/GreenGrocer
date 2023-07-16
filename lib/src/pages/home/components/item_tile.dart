67c6351d69a8d64c8730b9f4f9ef99900d511c1d
Davih Ferraz <davihlferraz@hotmail.com>
2023-07-16 15:43:28 -0300

    Formatando preco para Reais e adiconando botao para adicionar no carrinho

--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -1,66 +1,94 @@
 import 'package:flutter/material.dart';
 import 'package:green_grocer/src/models/item_model.dart';
 import 'package:green_grocer/src/pages/auth/config/custom_colors.dart';
+import 'package:green_grocer/src/services/util_services.dart';
 
 class ItemTile extends StatelessWidget {
-  const ItemTile({
+  ItemTile({
     super.key,
     required this.item,
   });
   final ItemModel item;
+  final UtilServices utilServices = UtilServices();
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 3,
-      shadowColor: Colors.grey.shade300,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(
-          30,
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            //Imagem
-            Expanded(
-              child: Image.asset(item.imageUrl),
+    return Stack(
+      children: [
+        Card(
+          elevation: 3,
+          shadowColor: Colors.grey.shade300,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              30,
             ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                //Imagem
+                Expanded(
+                  child: Image.asset(item.imageUrl),
+                ),
 
-            //Nome
-            Text(
-              item.itemName,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+                //Nome
+                Text(
+                  item.itemName,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                //Preco - Unidade
+                Row(
+                  children: [
+                    Text(
+                      utilServices.priceToCurrency(item.price),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: CustomColors.customSwatchColor,
+                      ),
+                    ),
+                    Text(
+                      '/${item.unit}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        Positioned(
+          right: 4,
+          top: 4,
+          child: GestureDetector(
+            child: Container(
+              height: 40,
+              width: 35,
+              decoration: BoxDecoration(
+                color: CustomColors.customSwatchColor,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: const Icon(
+                Icons.add_shopping_cart_outlined,
+                size: 20,
+                color: Colors.white,
               ),
             ),
-
-            //Preco - Unidade
-            Row(
-              children: [
-                Text(
-                  item.price.toString(),
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: CustomColors.customSwatchColor,
-                  ),
-                ),
-                Text(
-                  '/${item.unit}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey.shade500,
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+          ),
+        )
+      ],
     );
   }
 }
